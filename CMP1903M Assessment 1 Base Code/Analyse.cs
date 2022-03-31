f7e7dd23e9f14c3acea70402a204c57dec5d95ae
ChristianC03 <102488273+ChristianC03@users.noreply.github.com>
2022-03-31 12:30:45 +0100

    Add files via upload

---
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Analyse
+    {
+        //Handles the analysis of text
+
+        //Method: analyseText
+        //Arguments: string
+        //Returns: list of integers
+        //Calculates and returns an analysis of the text
+        public List<int> analyseText(string input)
+        {
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+            //2. Number of vowels
+            //3. Number of consonants
+            //4. Number of upper case letters
+            //5. Number of lower case letters
+            List<int> values = new List<int>();
+            
+            
+            //Initialise all the values in the list to '0'
+            
+            
+            for(int i = 0; i<5; i++)
+            {
+                values.Add(0);
+            }
+
+
+            foreach (char a in input)
+            {
+                if (a.ToString() == "*")
+                {
+                    break;
+                }
+            
+
+                
+                bool Lower_or_Uppercase = "abcdefghijklmnopqrstuvwxyz".IndexOf(a) >= 0;
+                if (Lower_or_Uppercase)
+                {
+                    values[4] += 1;
+                }
+                else if (a.ToString() != " " && a.ToString() != ".")
+                {
+                    values[3] += 1;
+                }
+
+
+
+                bool Consonant_or_Vowel = "aeiouAEIOU".IndexOf(a) >= 0;
+                if (Consonant_or_Vowel)
+                {
+                    values[2] += 1;
+                }
+                else if (a.ToString() != " " && a.ToString() != ".")
+                {
+                    values[1] += 1;
+                } 
+
+
+
+
+                bool Sentences = input.IndexOf(".") >= 0;
+                if (a.ToString() == ".")
+                {
+                    values[0] += 1;
+                }
+                if (Sentences == false)
+                {
+                    values[0] == 1;
+                }   
+            }
+
+            return values;
+        }
+    }
+}
---
+++ CMP1903M Assessment 1 Base Code/Program.cs
@@ -0,0 +1,68 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            
+            
+            //Local list of integers to hold the first five measurements of the text
+            
+            List<int> parameters = new List<int>();
+
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+            
+            Input i = new Input();
+            string choice = "";
+            string text = "";
+
+            
+            while (choice != "1" && choice != "2")
+            {
+                Console.Write("Would you like Option 1 or 2? : ");
+                choice = Console.ReadLine();
+            }
+            
+            
+            
+            if (choice == "1")
+            {
+                text = i.manualTextInput();
+            }
+
+
+
+            //Create an 'Analyse' object
+            //Pass the text input to the 'analyseText' method
+            
+            Analyse a = new Analyse();
+
+            //Receive a list of integers back
+            
+            List<int> values = a.analyseText(text);
+
+
+
+            //Report the results of the analysis
+            Console.WriteLine("upper-case letters: " + values[0]);
+            Console.WriteLine("lower-case letters: " + values[1]);
+            Console.WriteLine("vowels: " + values[2]);
+            Console.WriteLine("consonants: " + values[3]);
+            Console.WriteLine("sentences: " + values[4]);
+
+            //TO ADD: Get the frequency of individual letters?
+
+           
+        }
+    }
+}
